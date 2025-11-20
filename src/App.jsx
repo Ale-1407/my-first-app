8dcfb2f958cf0816132f23ffc07eaa171e1062d1
Ale-1407 <alessiacarrera07@gmail.com>
2025-11-20 01:02:50 +0100

    style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
-      <h1 className='welcome_title'>Hello World!</h1>
+      <h1 className='space welcome_title'>Hello World!</h1>
+      <div>
+        <label htmlFor="input" className='space first-label'>Inserisci del testo:</label>
+        <input type="text" id='input'className='space first-input' placeholder='Scrivi...'/>
+      </div>
     </>
   )
 }

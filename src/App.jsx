9709001eae4f84ff3b0ec8a775ef62b4ac0f39d5
Ale-1407 <alessiacarrera07@gmail.com>
2025-11-21 00:18:43 +0100

    list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Header from './components/Header'
+import List from './components/List'
 import Navbar from './components/Navbar'
 
 function App() {
+
+  const nomi = ['Alessia', 'Federico', 'Simone', 'Sara'];
 
   return (
     <>
       <Navbar />
       <Header />
+      <List nomi={nomi} />
       <div id='box'>
         <h1 className='space welcome_title'>Hello World!</h1>
         <label htmlFor="input" className='space first-label'>Inserisci del testo:</label>

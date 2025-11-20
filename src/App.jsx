8582d53c014c22a786000e12562d21ddff585d6a
Ale-1407 <alessiacarrera07@gmail.com>
2025-11-20 23:53:31 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1 className='space welcome_title'>Hello World!</h1>
       <div>
         <label htmlFor="input" className='space first-label'>Inserisci del testo:</label>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return (
+        <nav className="custom-nav space">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="https://github.com/">About</a>
+                </li>
+            </ul>  
+        </nav>
+    )
+}

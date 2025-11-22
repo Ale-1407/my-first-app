3d840e2e35872c6c07a0b815662384614a6947f2
Ale-1407 <alessiacarrera07@gmail.com>
2025-11-22 17:20:42 +0100

    onSubmit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 
 export default function Form() {
+
+    const [userName, setUserName] = useState("");
+    const [userMail, setUserMail] = useState("");
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+
+        console.log(userName);
+        console.log(userMail);    
+    }
+
+
     return (
         <>
-            <form className="space">
+            <form onSubmit={handleSubmit}  className="space">
                 <div className="mb-3 form-floating">
-                    <label htmlFor="userName" className="" >Name:</label>
-                    <input type="text" id="userName" placeholder="Your name" className="form-control w-25" />
+                    <label htmlFor="userName" >Name:</label>
+                    <input type="text" id="userName" placeholder="Your name" className="form-control w-25" 
+                    value={userName} onChange={(ev) => setUserName(ev.target.value)} />
                 </div>
                 <div className="mb-3 form-floating">
-                    <label htmlFor="userMail" className="" >Mail:</label>
-                    <textarea type="text" id="userMail" placeholder="Your message" className="form-control w-25"></textarea>
+                    <label htmlFor="userMail" >Mail:</label>
+                    <textarea type="text" id="userMail" placeholder="Your message" className="form-control w-25" 
+                    value={userMail} onChange={(ev) => setUserMail(ev.target.value)} ></textarea>
                 </div>
                 <div>
-                    <button type="button" class="btn btn-outline-light">Light</button>
+                    <button type="submit" className="btn btn-outline-light">Aggiungi</button>
                 </div>
             </form>
         </>
